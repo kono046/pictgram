16f3d06a5056633f2e3ad4e86080fb149d592e90
井上滉之伸 <kono.greeeeen.rion021@gmail.com>
2020-11-08 02:33:37 +0000

    ruby課題４

---
+++ app/models/concerns/rails04.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  #名前の長さに１５文字までの制限
+  validates :name, length: { maximum: 15}
+  
+  #メアドの正規表現
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  
+  #半角6~12文字で英大文字・英小文字・数字それぞれ１文字以上含む
+VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+  validates :password, presence: true
+  #パスワードの文字数制限
+  validates :password, length: { in: 8..32}
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"
+  
+  has_secure_password
+end
